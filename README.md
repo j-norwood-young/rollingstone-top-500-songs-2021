cd87351f6e511b5e734b07b396ddbc247876eeb5
Jason Norwood-Young <jason@10layer.com>
2021-09-30 16:55:38 +0200

    All the stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rolling Stone's Top 500 Songs 2021
+
+## This repository includes the raw data, a parser, and the cleaned data in JSON
+
+You can find the compiled data under `/data`, in both compressed and readable JSON formats. 
+
+The original data scraped from Rolling Stone is available in the `/raw` directory.
+
+The source code to parse the data is available under `/src`.
--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
-import fs from "fs";
+import fs from "fs"
+import path from "path"
 
+const songmapper = song => {
+    try {
+        const parts = song.subtitle.replace("\r\n", "").replace("\\r\\n", "").match(/<div class="rs-list-item--year">(\d*)<\/div>.*<span class="rs-list-item--credits__names">(.*)(<\/span>)/)
+        return {
+            id: song.ID,
+            position: song.positionDisplay,
+            title: song.title,
+            year: parts[1],
+            writers: parts[2].split(", "),
+            // subtitle: song.subtitle,
+            slug: song.slug,
+            description: song.description,
+            image: song.image,
+            appleSongID: song.appleSongID
+        }
+    } catch(err) {
+        console.log(song.subtitle);
+    }
+}
+
+const main = async () => {
+    const files = fs.readdirSync("./raw")
+    let songs = []
+    for (let file of files) {
+        const json = JSON.parse(fs.readFileSync(path.join("./raw", file)));
+        songs = [...json.gallery, ...songs]
+    }
+    songs = songs.map(songmapper).sort((a, b) => b.position - a.position);
+    fs.writeFileSync(path.join("./data", "rollingstone-top-500-songs.json"), JSON.stringify(songs, null, "   "));
+    fs.writeFileSync(path.join("./data", "rollingstone-top-500-songs.min.json"), JSON.stringify(songs));
+    console.log(`Wrote ${songs.length} songs`)
+}
+main()
